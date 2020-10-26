871f83722623dd570ac3b6228f0be92e4018b046
EsportsKing <divpadsmitais@gmail.com>
2020-10-26 15:57:55 +0200

    Task 2 and 3 finished

--- src/selenium/sample/Sample3Task.java
+++ src/selenium/sample/Sample3Task.java
@@ -36,29 +36,48 @@ public class Sample3Task {
 
     @Test
     public void assertEqualsTask() throws Exception {
-//         TODO:
 //         check how many element with class "test" there are on page (5)
+        System.out.println(driver.findElements(By.className("test")).size());
+//        check there are 5 elements with class "test"
+        int expected = 5;
+        int actual = driver.findElements(By.className("test")).size();
+        assertEquals(expected, actual);
 //         check that value of second button is "This is also a button"
+        // probably meant for the next task but test was performed anyway
+        String expectedStr = "This is also a button";
+        String actualStr = driver.findElement(By.name("randomButton2")).getAttribute("value");
+        assertEquals(expectedStr, actualStr);
     }
 
     @Test
     public void assertTrueTask() throws Exception {
-//         TODO:
 //         check that it is True that value of second button is
 //         "this is Also a Button" if you ignore Caps Locks
+        String expected = "this is Also a Button";
+        String actual = driver.findElement(By.name("randomButton2")).getAttribute("value");
+        try {
+            assertTrue(expected.equalsIgnoreCase(actual));
 //         fail with custom error message:
+        } catch (AssertionError e) {
+            System.err.println("Button text mismatch expected value");
+            e.printStackTrace();
+        }
     }
 
     @Test
     public void assertFalseTask() throws Exception {
-//         TODO:
 //        check that it is False that value of second button is "This is a button"
+        String notExpected = "This is a button";
+        String actual = driver.findElement(By.name("randomButton2")).getAttribute("value");
+        assertFalse(notExpected.equals(actual));
     }
 
     @Test
     public void failTask() throws Exception {
-//        TODO:
 //        check that none of items with class "test"
 //        contain number 190
+        int testElements = driver.findElements(By.className("test")).size();
+
+
     }
 }
